84a29e9b0a5a466215f7def6c3cc3cb033a4189f
Gustavo Vieira <gustavo.vieira2001@outlook.com>
2023-04-13 09:09:33 -0300

    Remove cached file of file that is in gitignore

--- src/main/kotlin/com/gacagebot/constants/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.gacagebot.constants
-
-object Constants {
-    const val BOT_TOKEN: String = "Your bot token here"
-    const val DJ_GCAGE: String = "*DJ Gcage*"
-    const val CHANNEL_ID: String = "If necessary"
-}
