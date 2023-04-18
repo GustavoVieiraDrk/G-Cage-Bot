b70d7fcbdcd6be972fd45bbc62bd8685de31e083
Gustavo Vieira <gustavo.vieira2001@outlook.com>
2023-04-18 20:36:27 -0300

    Change to enum class to get strings values

--- src/main/kotlin/com/gacagebot/constants/Secrets.kt
+++ src/main/kotlin/com/gacagebot/constants/Secrets.kt
@@ -1,7 +1,6 @@
 package com.gacagebot.constants
 
 enum class Secrets(val getString: String) {
-    BOT_TOKEN("MTAzNDI3MjAxNTQxNjMxNTk2NQ.GYBZ5D.ZUeLI-03P6_e_WP2J-9RA-2ljj1lZmKkQi_Aj0"),
-    DJ_G_CAGE("*Gcage DJ*"),
+    BOT_TOKEN("Paste your bot token here"),
     CHANNEL_ID("Use if necessary")
 }
