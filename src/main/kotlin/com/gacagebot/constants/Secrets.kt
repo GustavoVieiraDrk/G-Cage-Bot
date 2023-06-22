891cf2d782b87c96e902fa1787dd6cd9d41092ec
Gustavo Vieira <gustavo.vieira2001@outlook.com>
2023-06-22 20:23:39 -0300

    Add tenor api key.

--- src/main/kotlin/com/gacagebot/constants/Secrets.kt
+++ src/main/kotlin/com/gacagebot/constants/Secrets.kt
@@ -1,6 +1,7 @@
 package com.gacagebot.constants
 
 enum class Secrets(val getString: String) {
-    BOT_TOKEN("Paste your bot token here"),
-    CHANNEL_ID("Use if necessary")
+    BOT_TOKEN("your-bot-token"),
+    CHANNEL_ID("Use if necessary"),
+    TENOR_API_KEY("your-tenor-api-key")
 }
